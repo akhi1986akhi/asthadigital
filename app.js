c7e6a799393d706a6d354a4e6e0eb1dce6ffd92c
akhi1986akhi <akhi1986@hotmail.com>
2022-06-20 22:14:08 +0530

    contact page updated

--- app.js
+++ app.js
@@ -2,26 +2,102 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const jobs = require('./models/latestjob')
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
 const path = require('path');
+const nodemailer = require('nodemailer');
 const portname = process.env.PORT || 3000;
-mongoose.connect('mongodb+srv://akhi1986akhi:5ZT2CNDB7K3mPjwY@cluster0.wucuh.mongodb.net/mydb?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://akhi1986akhi:5ZT2CNDB7K3mPjwY@cluster0.wucuh.mongodb.net/mydb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(()=>{
+}).then(() => {
     console.log("database connected");
 });
 // app.use access public folder assets
 app.use('/assets', express.static('assets'));
 app.set('view engine', 'ejs');
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.render('pages/index');
 })
-app.get('/latestJob',function(req,res){
+app.get('/latestJob', function (req, res) {
     jobs.find({}).then((data) => {
         res.render('pages/latestJob', { records: data });
     })
 })
+app.get('/latestJobForm', function (req, res) {
+    res.render('pages/latestjobForm');
+})
+app.use(express.urlencoded({ extended: false }))
+app.post('/latestJobForm', jsonParser, function (req, res) {
+    console.log(req.body.title);
+    const createNewForm = new jobs({
+        date: req.body.date,
+        post: req.body.post,
+        postname: req.body.postname,
+        title: req.body.title,
+        fdate: req.body.fdate,
+        tdate: req.body.tdate,
+        gfee: req.body.gfee,
+        ofee: req.body.ofee,
+        scfee: req.body.scfee,
+        stfee: req.body.stfee,
+        pfee: req.body.pfee,
+        efee: req.body.efee
+    })
+    createNewForm.save().then(async (result) => {
+        console.log(result);
+        res.redirect('/latestJob');
+    })
 
-app.listen(portname,()=>{
+})
+app.get('/contact', (req, res) => {
+    res.render('pages/contact');
+})
+
+
+app.post('/contact', jsonParser, (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const contact = req.body.contact;
+    const message = req.body.message;
+
+    var transporter = nodemailer.createTransport({
+        host: 'smtpout.secureserver.net',
+        port: 80,
+        secure: false,
+        requireTLS: true,
+        auth: {
+            user: 'info@asthadigital.co.in',
+            pass: 'King$420king'
+        }
+    });
+    var mailOptions = {
+        from: 'info@asthadigital.co.in',
+        to: 'akhi1986@hotmail.com',
+        subject: ' CUSTOMER INQERY | '+ name,
+        text: `
+        
+        Name: ${name}
+        E-mail: ${email}
+        Contact: ${contact}
+        
+        Message: 
+        ${message}
+        `
+    }
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Mail Sent successusfully", info.response);
+            res.render('pages/contactThankyou');
+        }
+    })
+    // res.send("Mail Sent successfully");
+
+})
+
+app.listen(portname, () => {
     console.log(`Application started successfully on port ${portname}`);
 });
